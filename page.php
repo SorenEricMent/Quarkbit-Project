1841d396e2cf702567182028bf455760b4298e6d
SorenEricMent <36105035+SorenEricMent@users.noreply.github.com>
2019-10-13 01:25:44 +0800

    BugFix again
    Last commit include bug that i did not aware.
    This page.php is the right one（commit 563f3978c389abb4605f9a829e333f0ed9c4780e）

--- page.php
+++ page.php
@@ -1,10 +1,9 @@
-<?php get_header(); ?>
-<?php get_sidebar(); ?>
+<?php get_header();?>
 <?php if(have_posts()) : ?>
-<?php while(have_posts()) : the_post(); ?> 
-	<?php the_post();
-	the_title();
-	the_content(); ?>
+<?php while(have_posts()) : the_post(); ?>
+	<?php the_title(); ?>
+<?php the_content(); ?>
 <?php endwhile; ?>
 <?php endif; ?>
-<?php get_footer(); ?>
+<?php get_sidebar();?>
+<?php get_footer();?>
